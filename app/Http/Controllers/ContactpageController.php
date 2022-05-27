4c300d5de5a667ecc2c259159ed36d553111b69e
Roman Tkachenko <roman2003tk@gmail.com>
2022-05-27 15:27:43 +0300

    added models and controllers

---
+++ app/Http/Controllers/ContactpageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactpageController extends Controller
+{
+    public function index(){
+        return view('pages.contactpage');
+    }
+}
