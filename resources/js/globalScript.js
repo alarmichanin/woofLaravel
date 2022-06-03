a45fb83d415b17bc14355e65dc07b44fa86e9f76
Roman Tkachenko <roman2003tk@gmail.com>
2022-06-03 11:32:11 +0300

    added script for adding class to the block needed to animate

--- resources/js/globalScript.js
+++ resources/js/globalScript.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
+    addAnimation();
+
     $(".barI .fa-bars").on("click", () => {
         $(".hamburger_menu").css("visibility", "visible").css("left", "0")
     })
@@ -6,3 +8,39 @@ $(document).ready(() => {
         $(".hamburger_menu").css("visibility", "hidden").css("left", "-100%")
     })
 });
+
+function addAnimation() {
+    const animItems = $('._anim-items');
+    if (animItems.length > 0) {
+        window.addEventListener('scroll', animOnScroll);
+
+        function animOnScroll() {
+            for (let i = 0; i < animItems.length; i++) {
+                const animItem = animItems[i];
+                const animItemHeight = animItem.offsetHeight;
+                //TODO: reread this functionality
+                const animItemOffset = offset(animItem).top / 1.5;
+                const animStart = 1;
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add("_active");
+                } else {
+                    animItem.classList.remove("_active");
+                }
+            }
+        }
+
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+        }
+
+        animOnScroll();
+    }
+}
+
