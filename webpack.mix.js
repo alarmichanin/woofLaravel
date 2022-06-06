4d46d85e858ab49e2a4d02bb9fd396cefa223402
Roman Tkachenko <roman2003tk@gmail.com>
2022-06-06 23:37:58 +0300

    added new entry point for js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/login.js','public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
 mix.less('resources/less/app.less', 'public/less')
+    .less('resources/less/login.less','public/less');
