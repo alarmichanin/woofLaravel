c687560f8f878d3f318c6d091f2679f72a87fec6
Roman Tkachenko <roman2003tk@gmail.com>
2022-06-15 14:02:09 +0300

    changed structure for script adding

--- resources/js/globalScript.js
+++ resources/js/globalScript.js
@@ -1,34 +1,7 @@
+import {hamburgerMenu} from './globalLogic/hamburgerMenu';
+import {addAnimation} from './globalLogic/addAnimation';
+
 $(document).ready(() => {
     addAnimation();
-
-    $(".barI .fa-bars").on("click", () => {
-        $(".hamburger_menu").css("visibility", "visible").css("left", "0")
-    })
-    $(".barI .close_menu").on("click", () => {
-        $(".hamburger_menu").css("visibility", "hidden").css("left", "-100%")
-    })
+    hamburgerMenu();
 });
-
-function addAnimation() {
-    const animItems = $('._anim-items');
-    if (animItems.length > 0) {
-        window.addEventListener('scroll', animOnScroll);
-
-        function animOnScroll() {
-            for (let i = 0; i < animItems.length; i++) {
-                const animItem = animItems[i];
-                let windowHeight = window.innerHeight;
-                let elementTop = animItem.getBoundingClientRect().top;
-                let elementVisible = 75;
-                if (Math.abs(elementTop) < windowHeight - elementVisible) {
-                    animItem.classList.add("_active");
-                } else {
-                    animItem.classList.remove("_active");
-                }
-            }
-        }
-
-        animOnScroll();
-    }
-}
-
