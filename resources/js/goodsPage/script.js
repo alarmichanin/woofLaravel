c96f722bdcadd901ad274d3fac52aab6fa5ee563
Roman Tkachenko <roman2003tk@gmail.com>
2022-06-17 23:20:57 +0300

    changed script for goodsPage with filtering criteria

--- resources/js/goodsPage/script.js
+++ resources/js/goodsPage/script.js
@@ -1,6 +1,10 @@
 $(document).ready(() => {
     $("div.card,div.product").on("click", (e) => {
-        window.location.href = "../productPage"+"";
+        window.location.href = "../productPage/" + e.currentTarget.id;
+    })
+    $(".category").on("click",(e)=>{
+        window.location.href = "/goodsPage/" + e.currentTarget.dataset.filter;
+
     })
     $(".filter i").on("click", () => {
         $(".category_menu").css("visibility", "visible").css("left", "0")
