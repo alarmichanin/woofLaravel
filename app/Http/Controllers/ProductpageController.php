b3ab6160ae06512a1697928f39b3c0301d72810d
Roman Tkachenko <roman2003tk@gmail.com>
2022-06-17 23:10:30 +0300

    added method for buy action at the product page

--- app/Http/Controllers/ProductpageController.php
+++ app/Http/Controllers/ProductpageController.php
@@ -2,11 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductpageController extends Controller
 {
-    public function index(){
-        return view('pages.productpage');
+    public function index($slug)
+    {
+        $product = Product::with('brand', 'category', 'size', 'photos')->where("MPN", $slug);
+        return view('pages.productpage', [
+            "product" => $product->get()
+        ]);
+    }
+
+    public function buy($slug, $count)
+    {
+        if (!Auth::check()) {
+            return redirect(route('user.profile'));
+        }
+        $product_id = Product::where('MPN', $slug)->first()->id;
+        $user_id = Auth::id();
+        $order = new Order;
+        $order->count = $count;
+        $order->product_id = $product_id;
+        $order->users_id = $user_id;
+        $order->save();
+        return redirect(route('goods'));
     }
 }
