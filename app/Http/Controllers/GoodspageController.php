7082bfe07882dd72e07b20e2d5ccd8b3090d95c1
Roman Tkachenko <roman2003tk@gmail.com>
2022-06-17 23:09:30 +0300

    changed index method in goodspagecontroller for sorting goods

--- app/Http/Controllers/GoodspageController.php
+++ app/Http/Controllers/GoodspageController.php
@@ -4,18 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class GoodspageController extends Controller
 {
-    public function index()
+    public function index($category = null)
     {
-        $products = Product::with(['brand', 'category', 'size', 'photos' => function (HasMany $query) {
-            $query->where('isGeneral', 1);
-        }]);
+        if ($category) {
+            $category_id = explode("-", $category)[1];
+            $products = Product::whereHas('category', function ($query) use ($category_id) {
+                $query->where('id', $category_id);
+            })->with(['brand', 'size', 'category', 'photos' => function (HasMany $query) {
+                $query->where('isGeneral', 1);
+            }]);
+        } else {
+            $products = Product::with(['brand', 'category', 'size', 'photos' => function (HasMany $query) {
+                $query->where('isGeneral', 1);
+            }]);
+        }
         return view('pages.goodspage', [
             'categories' => Category::all(),
-            'products' => $products->simplePaginate(1),//24
+            'products' => $products->simplePaginate(8),
             'popular' => $products->where('popular', 1)->get(),
         ]);
     }
